be124ac369ed923c994895e884e489a9cdbc897d
Aaron Sum <sumsx03@gmail.com>
2018-03-14 12:31:21 +0100

    add comments for the code.

--- src/com/Personify/startup/Main.java
+++ src/com/Personify/startup/Main.java
@@ -3,8 +3,23 @@ package com.Personify.startup;
 import java.io.IOException;
 import com.Personify.textView.TextInterface;
 
+/**
+ * Initialized the Personify program.
+ * 
+ * @author aaronsum
+ * @version 2.0, 2018-03-13
+ */
 public class Main {
-	public static void main(String[] args) throws IOException{
+
+	/**
+	 * Startup the system operation loop.
+	 * 
+	 * @param args
+	 *            There are no command line expected.
+	 * @throws IOException
+	 *             If an input or output error occurred.
+	 */
+	public static void main(String[] args) throws IOException {
 		TextInterface view = new TextInterface();
 		view.startup();
 	}
