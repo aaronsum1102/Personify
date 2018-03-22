157d14d37fd0b9705e43bc2de6226cc274043fc6
Aaron Sum <sumsx03@gmail.com>
2018-03-22 11:39:35 +0100

    Implement new features for showing tasks.
    Changes in this commit are as follow:
    1. Add the following features:
        - show all tasks.
        - show tasks to complete.
        - show tasks that were done.
        - show overdue tasks.
        - Go back to previous menu.
    2. Add new menu for showing tasks.
    3. method for filtering tasks based on specify status.
    4. method for filtering task to be done.
    5. methods in controller and view to initiate the filter methods.
    6. Refactor view class to reduce repeated code for operation by implementation of stack.

--- src/com/Personify/textView/Command.java
+++ src/com/Personify/textView/Command.java
@@ -5,16 +5,21 @@ import java.util.List;
 
 import com.Personify.integration.FileIO;
 import com.Personify.integration.FilePath;
+import com.Personify.integration.Messenger;
 
 public class Command {
+	private Messenger messenger;
 	private List<String> commands;
 	private List<String> mainCommands;
 	private List<String> editCommands;
+	private List<String> showTasksCommands;
 	
-	public Command() throws IOException {
+	public Command(Messenger messenger) throws IOException {
+		this.messenger = messenger; 
 		commands = getCommandsFromFile();
 		mainCommands = commands.subList(getMainCommandsMinNumber(), getMainCommandsMaxNumber());
 		editCommands = commands.subList(getSubCommandsMinNumber(), commands.size());
+		showTasksCommands = commands.subList(getShowTasksCommandsMinNumber(), getShowTasksCommandsMaxNumber());
 	}
 	
 	private List<String> getCommandsFromFile() throws IOException {
@@ -24,22 +29,49 @@ public class Command {
 	}
 	
 	private int getMainCommandsMaxNumber() {
-		return commands.indexOf("edit menu");
+		return commands.indexOf("show all tasks");
 	}
 	
 	private int getMainCommandsMinNumber() {
 		return commands.indexOf("main menu") + 1;
 	}
 	
-	public List<String> getMainCommands() {
-		return mainCommands;
+	public void getMainCommands() {
+		mainCommands.stream()
+					.forEach(choice -> messenger.addMessage(choice));
+	}
+	
+	public int getMainCommandsSize() {
+		return mainCommands.size();
 	}
 	
 	private int getSubCommandsMinNumber() {
 		return commands.indexOf("edit menu") + 1;
 	}
 
-	public List<String> getEditCommands() {
-		return editCommands;
+	public void getEditCommands() {
+		editCommands.stream()
+					.forEach(choice -> messenger.addMessage(choice));
+	}
+	
+	public int getEditCommandsSize() {
+		return editCommands.size();
+	} 
+	
+	private int getShowTasksCommandsMinNumber() {
+		return commands.indexOf("show all tasks") + 1;
+	}
+	
+	private int getShowTasksCommandsMaxNumber() {
+		return commands.indexOf("edit menu");
+	}
+	
+	public void getShowTasksCommands() {
+		showTasksCommands.stream()
+							.forEach(choice -> messenger.addMessage(choice));
+	}
+	
+	public int getShowTasksCommandsSize() {
+		return showTasksCommands.size();
 	}
 }
